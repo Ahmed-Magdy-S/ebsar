12f2bdc33b4e83d0b608c56472c9e17eb73fa953
Ahmed-Magdy-S <ahmed.magdy.s.19@gmail.com>
2026-03-14 00:03:55 +0200

    init project

---
+++ src/main/java/com/az/analytics/config/DbConfig.java
@@ -0,0 +1,15 @@
+package com.az.analytics.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DbConfig {
+
+    @Bean
+    public DataSource dataSource(){
+
+    }
+}
