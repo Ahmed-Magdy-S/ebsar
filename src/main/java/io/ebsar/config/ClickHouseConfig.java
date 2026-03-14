17360f96763febec338cbd379ec6a6f30b71045e
Ahmed-Magdy-S <ahmed.magdy.s.19@gmail.com>
2026-03-14 04:23:01 +0200

    feat: Add ClickHouse database integration with new configuration, dependencies, and a Docker service.

---
+++ src/main/java/io/ebsar/config/ClickHouseConfig.java
@@ -0,0 +1,24 @@
+package io.ebsar.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class ClickHouseConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "clickhouse.datasource")
+    public DataSource clickHouseDataSource() {
+        return new HikariDataSource();
+    }
+
+    @Bean
+    public JdbcTemplate clickHouseJdbcTemplate(DataSource clickHouseDataSource) {
+        return new JdbcTemplate(clickHouseDataSource);
+    }
+}
